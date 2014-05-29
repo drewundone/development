75936b4775c6047cbfd2eee4c394daaef35bd90f
drewundone <dmason@bbasolutions.com>
2014-05-28 22:07:20 -0500

    Fixed and added grandpa..

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,26 @@
-puts 'Hello, sonny?!?'
-puts 'Say something to grandma:'
-answer = gets.chomp
-while answer != answer.upcase
-  if
-  puts 'What?' + answer
+
+puts 'Grandma: HEY THERE, SONNY! GIVE GRANDMA A KISS!'
+puts 'Grandpa: Type what you would like to say to Grandma:'
+while true
+  puts 'You: '
+  said = gets.chomp
+  if said == 'BYE'
+    puts 'Grandma and Grandpa: BYE!'
+    break
   end
-puts 'yay!'
+  if said == 'How did you get to school?'
+    miles = 1 + rand(20..75)
+    puts  'Grandpa: We walked ' + miles.to_s + ' miles to school! UPHILL! BOTH WAYS!!'
+    puts 'Grandma: HAVE FUN AT SCHOOL!'
+  break
+  end
+  if said != said.upcase
+    puts 'Grandma: WHAAT? SPEAK UP!'
+    puts 'Grandpa: You are talking to quiet...'
+  else
+    random_year_grandma = 1930 + rand(21)
+    puts 'Grandma: NO, NOT SINCE ' + random_year_grandma.to_s + '!'
+    random_year_grandpa = 1900 + rand(34)
+    puts 'Grandpa: She\'s been giving that answer since ' + random_year_grandpa.to_s + '!'
+  end
 end
