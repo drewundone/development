a2cb794ff6bab8df64b5694bed4cc832c1113404
Drew Mason <dmason@bbasolutions.com>
2014-07-05 21:11:54 -0500

    yup...

---
+++ Arrays.rb
@@ -0,0 +1,9 @@
+==begin
+names = ['Ada', 'Belle', 'Chris', 'Drew']
+
+puts names
+puts
+puts names[0]
+puts names[1]
+puts names[2]
+puts names[3]
---
+++ manywords.rb
@@ -0,0 +1,11 @@
+
+words = []
+array = words.length
+puts 'Please enter a word:'
+words.push gets.chomp
+if words.length != array
+  'Please enter another word'
+  else
+end
+
+puts words
